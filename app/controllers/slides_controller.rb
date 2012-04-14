ffd2eb4eb414a10b10bf4049b5565bf7eb0163f4
Brad Chase <bosco87@gmail.com>
2012-04-14 03:30:32 -0400

    SlidesController

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -1,2 +1,31 @@
 class SlidesController < ApplicationController
+  before_filter :check_for_round_id, :only => [:index,:create]
+
+  respond_to :json
+
+  def index
+    @slides = Round.find(@round_id).slides
+    respond_with @slides.to_json
+  end
+
+  def create
+    respond_with Slide.create(params[:slide]).to_json
+  end
+
+  def update
+    respond_with Slide.update(params[:id],params[:slide]).to_json
+  end
+
+  def destroy
+    respond_with Slide.destroy(params[:id]).to_json
+  end
+
+private
+  def check_for_round_id
+    if not @round_id = params[:round_id]
+      respond_with :bad_request
+    else
+      params[:slide][:round_id] = @round_id if params[:slide]
+    end
+  end
 end
--- spec/controllers/slides_controller_spec.rb
+++ spec/controllers/slides_controller_spec.rb
@@ -1,5 +1,97 @@
 require 'spec_helper'
 
 describe SlidesController do
+  before(:each) { Slide.any_instance.stub(:content).and_return('') }
+
+  describe 'GET index' do
+    it 'should throw a 406 if there is no round_id' do
+      get :index, {}, {} 
+      response.status.should == 406
+    end
+
+    it 'should show Slides for a Round' do
+      @round = Factory(:round)
+      @slide = Factory(:slide)
+      @round.slides << @slide
+      params = { :round_id => @round.to_param }
+
+      get :index, params, {}
+      assigns(:slides).should == [@slide]
+    end
+  end
+
+  describe 'GET show' do
+    it 'should not throw a 406 if there is no slide_id' do
+      pending 'no idea, try after you have more implemented'
+      put :update, { :slide => Factory.build(:slide) }, {} 
+      response.status.should_not == 406
+    end
+
+    it 'should show Slides for a Round' do
+      pending 'need for a show action'
+      @round = Factory(:round)
+      @slide = Factory(:slide)
+      @round.slides << @slide
+      params = { :round_id => @round.to_param }
+
+      get :show, params, {}
+      assigns(:slides).should == [@slide]
+    end
+  end
+
+  describe 'POST create' do
+    it 'should throw a 406 if there is no round_id' do
+      post :create, {}, {} 
+      response.status.should == 406
+    end
+
+    it 'should create a new Slide' do
+      @round = Factory(:round)
+      params = { :round_id => @round.to_param, :slide => {} }
+
+      expect {
+        post :create, params, {}
+      }.to change(Slide, :count).by(1)
+    end
+  end
+
+  describe 'PUT update' do
+    it 'should not throw a 406 if there is no slide_id' do
+      pending 'no idea, try after you have more implemented'
+      put :update, { :slide => Factory.build(:slide) }, {} 
+      response.status.should_not == 406
+    end
+
+    it 'should update the Slide whose id was passed in' do
+      @slide = Factory(:slide)
+
+      id = @slide.to_param
+      round_id = 1 
+      params = { 
+        :id => id,
+        :slide  => {:round_id => round_id} 
+      }
+
+      put :update, params, {}
+      Slide.find(id).round_id.should == round_id 
+    end
+  end
+
+  describe 'DELETE destroy' do
+    it 'should not throw a 406 if there is no slide_id' do
+      pending 'no idea, try after you have more implemented'
+      delete :destroy, {}, {} 
+      response.status.should_not == 406
+    end
+
+    it 'should destroy the Slide whose id was passed in' do
+      @slide = Factory(:slide)
+      params = { :id => @slide.to_param }
+
+      expect {
+        delete :destroy, params, {}
+      }.to change(Slide, :count).by(-1)
+    end
+  end
 
 end
