29e1905a1e9837433c0f2478e7c566a5811c67bb
Brad Chase <bosco87@gmail.com>
2012-04-24 19:35:35 -0400

    heroku works! (read: appears to) details in scratch/heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 protected
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
-      username == "admin" && password == "test"
+      username == "rounds_dev_server" && password == "YE%:Py+,oT\]dPWD"
     end
   end
 end
