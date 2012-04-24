f05af328e44727b19e6eb7a7342eb2e8ccee3c6b
Brad Chase <bosco87@gmail.com>
2012-04-24 18:04:32 -0400

    basic http auth for push to heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_filter :authenticate
+
   protect_from_forgery
+
+protected
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "admin" && password == "test"
+    end
+  end
 end
