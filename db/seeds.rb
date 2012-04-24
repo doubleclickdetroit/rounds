9221640ac52e9ce95b1cb5c589764af929929599
Brad Chase <bosco87@gmail.com>
2012-04-24 00:40:12 -0400

    added users to db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,9 @@
 puts "** Begin seeding"
+
+def random_user
+  @users[rand(@users.size)]
+end
+
 def random_text
   @lines ||= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id vulputate leo. Suspendisse vulputate libero eu leo ornare adipiscing. Cras mattis tristique mollis. Aenean et metus neque, nec porta lacus. Sed quis semper nisl. Nunc vel mi sem, vitae imperdiet metus. Suspendisse purus metus, pulvinar ac scelerisque et, rutrum ultrices urna. Sed faucibus placerat turpis, sed luctus odio aliquam nec. Integer justo diam, rhoncus in malesuada ac, eleifend eget libero. Praesent congue suscipit urna eu semper. Vestibulum velit nisl, porta sit amet aliquam faucibus, semper vel eros. Proin arcu massa, iaculis et malesuada dignissim, egestas eu diam. Vestibulum eleifend ante id nisl sollicitudin ac faucibus diam hendrerit. Vivamus vitae ipsum dui, et tincidunt felis. Nullam posuere odio egestas sapien ornare convallis. Nullam eget mi vel lorem mollis gravida. Mauris quam velit, malesuada nec tempor ac, dapibus quis augue. Suspendisse potenti.".split(/\./).map(&:strip)
 
@@ -6,6 +11,31 @@ def random_text
   (rand(4)+1).times { text << (@lines[rand(@lines.size)]+". ") }
   text
 end
+
+def add_arbitrary_comments_to(slide)
+  # comments
+  (rand(5)+1).times do
+    slide.comments << FactoryGirl.create(:comment, {:fid => random_user().fid, :text => random_text()})
+  end
+end
+
+
+
+##########################################
+################ Users ###################
+##########################################
+
+puts '  ** Users'
+
+@users = []
+
+@ammar = FactoryGirl.create(:user, :email => 'ammar@almakzumi.com', :password => 'ammaralmakzumi')
+@ben   = FactoryGirl.create(:user, :email => 'ben@babics.com', :password => 'benbabics')
+@brad  = FactoryGirl.create(:user, :email => 'brad@chase.com', :password => 'bradchase')
+
+@users << @ammar
+@users << @ben
+@users << @brad
 
 
 
@@ -15,7 +45,7 @@ end
 
 puts '  ** Round'
 
-@round = FactoryGirl.create(:round)
+@round = FactoryGirl.create(:round, :fid => random_user().fid)
 
 
 
@@ -26,13 +56,16 @@ puts '  ** Round'
 puts '    ** Slides'
 
 def make_sentence
-  params = { :round_id => @round.to_param }
+  fid = random_user().fid
+  params = { :round_id => @round.to_param, :fid => fid }
   FactoryGirl.create(:sentence, params) 
 end
 
 def make_picture
+  fid = random_user().fid
   pic = FactoryGirl.build(:picture, :with_file) 
   pic.round_id = @round.to_param
+  pic.fid = fid
   pic.save
 end
 
@@ -49,9 +82,7 @@ end
 puts '      ** Comments'
 
 @round.slides.each do |slide|
-  (rand 4).times do
-    slide.comments << FactoryGirl.create(:comment, :text => random_text)
-  end
+  add_arbitrary_comments_to(slide)
 end
 
 
