d816f7998db6e43edbc797cf11bb03a10cb96458
Brad Chase <bosco87@gmail.com>
2012-06-20 06:14:53 -0400

    const for app name

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ if defined?(Bundler)
 end
 
 module Draw
+  NAME = 'Rounds'
+
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
