63e8b705be240d0a8d589c8c50f855492a1b1b7f
Brad Chase <bosco87@gmail.com>
2012-06-27 04:35:57 -0400

    piddly changes

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,14 +3,16 @@ class Picture < Slide
     has_attached_file :file,
       path: ':attachment/:id/:style.:extension',
       storage: :s3,
-      s3_credentials: Rails.root.join('config','aws_s3.yml')
+      s3_credentials: AWS::CREDENTIALS,
+      default_url: '/assets/rails.png'
   else
-    has_attached_file :file, path: './public/system/:attachment/:id/:style.:extension'
+    has_attached_file :file, path: './public/system/:attachment/:id/:style.:extension', default_url: '/assets/rails.png'
   end
 
   def self.get_aws_credentials
     # security token service
     sts = AWS::STS.new(AWS::CREDENTIALS) 
+    # todo ^ keep this instance somewhere?
     sts.new_session()
   end
 
